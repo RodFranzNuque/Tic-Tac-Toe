a8e0dee90dda9e406857cfa6a2d24c6d504930a5
RodFranz Nuque <rodkolehiyo@gmail.com>
2022-06-03 20:33:11 +0800

    Minor Interface changes, Addition of Game title, Rules button, and minor design changes

--- TicTacToe_Rod/Form1.Designer.cs
+++ TicTacToe_Rod/Form1.Designer.cs
@@ -38,79 +38,143 @@
             this.row3column1 = new System.Windows.Forms.Button();
             this.row3column2 = new System.Windows.Forms.Button();
             this.row3column3 = new System.Windows.Forms.Button();
+            this.GameTitle = new System.Windows.Forms.Label();
+            this.Rules = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // row1column1
             // 
-            this.row1column1.Location = new System.Drawing.Point(119, 128);
+            this.row1column1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.row1column1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.row1column1.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.row1column1.ForeColor = System.Drawing.Color.Gold;
+            this.row1column1.Location = new System.Drawing.Point(166, 166);
             this.row1column1.Name = "row1column1";
-            this.row1column1.Size = new System.Drawing.Size(110, 110);
+            this.row1column1.Size = new System.Drawing.Size(88, 92);
             this.row1column1.TabIndex = 0;
-            this.row1column1.UseVisualStyleBackColor = true;
+            this.row1column1.UseVisualStyleBackColor = false;
             // 
             // row1column2
             // 
-            this.row1column2.Location = new System.Drawing.Point(263, 128);
+            this.row1column2.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.row1column2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.row1column2.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.row1column2.ForeColor = System.Drawing.Color.Gold;
+            this.row1column2.Location = new System.Drawing.Point(274, 166);
             this.row1column2.Name = "row1column2";
-            this.row1column2.Size = new System.Drawing.Size(110, 110);
+            this.row1column2.Size = new System.Drawing.Size(88, 92);
             this.row1column2.TabIndex = 1;
-            this.row1column2.UseVisualStyleBackColor = true;
+            this.row1column2.UseVisualStyleBackColor = false;
             // 
             // row1column3
             // 
-            this.row1column3.Location = new System.Drawing.Point(408, 128);
+            this.row1column3.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.row1column3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.row1column3.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.row1column3.ForeColor = System.Drawing.Color.Gold;
+            this.row1column3.Location = new System.Drawing.Point(377, 166);
             this.row1column3.Name = "row1column3";
-            this.row1column3.Size = new System.Drawing.Size(110, 110);
+            this.row1column3.Size = new System.Drawing.Size(88, 92);
             this.row1column3.TabIndex = 2;
-            this.row1column3.UseVisualStyleBackColor = true;
+            this.row1column3.UseVisualStyleBackColor = false;
             // 
             // row2column1
             // 
-            this.row2column1.Location = new System.Drawing.Point(119, 266);
+            this.row2column1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.row2column1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.row2column1.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.row2column1.ForeColor = System.Drawing.Color.Gold;
+            this.row2column1.Location = new System.Drawing.Point(166, 285);
             this.row2column1.Name = "row2column1";
-            this.row2column1.Size = new System.Drawing.Size(110, 110);
+            this.row2column1.Size = new System.Drawing.Size(88, 92);
             this.row2column1.TabIndex = 3;
-            this.row2column1.UseVisualStyleBackColor = true;
+            this.row2column1.UseVisualStyleBackColor = false;
             // 
             // row2column2
             // 
-            this.row2column2.Location = new System.Drawing.Point(263, 266);
+            this.row2column2.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.row2column2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.row2column2.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.row2column2.ForeColor = System.Drawing.Color.Gold;
+            this.row2column2.Location = new System.Drawing.Point(274, 285);
             this.row2column2.Name = "row2column2";
-            this.row2column2.Size = new System.Drawing.Size(110, 110);
+            this.row2column2.Size = new System.Drawing.Size(88, 92);
             this.row2column2.TabIndex = 4;
-            this.row2column2.UseVisualStyleBackColor = true;
+            this.row2column2.UseVisualStyleBackColor = false;
             // 
             // row2column3
             // 
-            this.row2column3.Location = new System.Drawing.Point(408, 266);
+            this.row2column3.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.row2column3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.row2column3.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.row2column3.ForeColor = System.Drawing.Color.Gold;
+            this.row2column3.Location = new System.Drawing.Point(377, 285);
             this.row2column3.Name = "row2column3";
-            this.row2column3.Size = new System.Drawing.Size(110, 110);
+            this.row2column3.Size = new System.Drawing.Size(88, 92);
             this.row2column3.TabIndex = 5;
-            this.row2column3.UseVisualStyleBackColor = true;
+            this.row2column3.UseVisualStyleBackColor = false;
             // 
             // row3column1
             // 
-            this.row3column1.Location = new System.Drawing.Point(119, 413);
+            this.row3column1.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.row3column1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.row3column1.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.row3column1.ForeColor = System.Drawing.Color.Gold;
+            this.row3column1.Location = new System.Drawing.Point(166, 400);
             this.row3column1.Name = "row3column1";
-            this.row3column1.Size = new System.Drawing.Size(110, 110);
+            this.row3column1.Size = new System.Drawing.Size(88, 92);
             this.row3column1.TabIndex = 6;
-            this.row3column1.UseVisualStyleBackColor = true;
+            this.row3column1.UseVisualStyleBackColor = false;
             // 
             // row3column2
             // 
-            this.row3column2.Location = new System.Drawing.Point(263, 413);
+            this.row3column2.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.row3column2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.row3column2.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.row3column2.ForeColor = System.Drawing.Color.Gold;
+            this.row3column2.Location = new System.Drawing.Point(274, 400);
             this.row3column2.Name = "row3column2";
-            this.row3column2.Size = new System.Drawing.Size(110, 110);
+            this.row3column2.Size = new System.Drawing.Size(88, 92);
             this.row3column2.TabIndex = 7;
-            this.row3column2.UseVisualStyleBackColor = true;
+            this.row3column2.UseVisualStyleBackColor = false;
             // 
             // row3column3
             // 
-            this.row3column3.Location = new System.Drawing.Point(408, 413);
+            this.row3column3.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.row3column3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.row3column3.Font = new System.Drawing.Font("Bauhaus 93", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.row3column3.ForeColor = System.Drawing.Color.Gold;
+            this.row3column3.Location = new System.Drawing.Point(377, 400);
             this.row3column3.Name = "row3column3";
-            this.row3column3.Size = new System.Drawing.Size(110, 110);
+            this.row3column3.Size = new System.Drawing.Size(88, 92);
             this.row3column3.TabIndex = 8;
-            this.row3column3.UseVisualStyleBackColor = true;
+            this.row3column3.UseVisualStyleBackColor = false;
+            // 
+            // GameTitle
+            // 
+            this.GameTitle.AutoSize = true;
+            this.GameTitle.Font = new System.Drawing.Font("Bauhaus 93", 49.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.GameTitle.ForeColor = System.Drawing.Color.OliveDrab;
+            this.GameTitle.Location = new System.Drawing.Point(82, 9);
+            this.GameTitle.Name = "GameTitle";
+            this.GameTitle.Size = new System.Drawing.Size(468, 96);
+            this.GameTitle.TabIndex = 9;
+            this.GameTitle.Text = "Tic-Tac-Toe";
+            this.GameTitle.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Rules
+            // 
+            this.Rules.BackColor = System.Drawing.Color.OliveDrab;
+            this.Rules.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.Rules.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Rules.ForeColor = System.Drawing.Color.Gold;
+            this.Rules.Location = new System.Drawing.Point(274, 108);
+            this.Rules.Name = "Rules";
+            this.Rules.Size = new System.Drawing.Size(94, 36);
+            this.Rules.TabIndex = 10;
+            this.Rules.Text = "Rules?";
+            this.Rules.UseVisualStyleBackColor = false;
+            this.Rules.Click += new System.EventHandler(this.button1_Click);
             // 
             // TicTacToe
             // 
@@ -118,6 +182,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gold;
             this.ClientSize = new System.Drawing.Size(656, 596);
+            this.Controls.Add(this.Rules);
+            this.Controls.Add(this.GameTitle);
             this.Controls.Add(this.row3column3);
             this.Controls.Add(this.row3column2);
             this.Controls.Add(this.row3column1);
@@ -132,6 +198,7 @@
             this.Text = "Tic-Tac-Toe";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -146,5 +213,7 @@
         private Button row3column1;
         private Button row3column2;
         private Button row3column3;
+        private Label GameTitle;
+        private Button Rules;
     }
 }
--- TicTacToe_Rod/Form1.cs
+++ TicTacToe_Rod/Form1.cs
@@ -21,5 +21,21 @@ namespace TicTacToe_Rod
         {
 
         }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rules_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Player X and O play in turns. The first player to have 3 marks on a row in any way (horizontal, Vertical, Diagonal) wins. " +
+                "If all the squares are filled and there are no 3 marks in a row, the game ends in a draw");
+        }
     }
 }
